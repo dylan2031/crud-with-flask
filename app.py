92e5a93816c906852788105ddf94c0799a5372a0
Dylan <dellis2031@gmail.com>
2025-03-28 18:11:37 -0400

    Created data.py as sample data to work with for now. Created posts page and route, which successfully displays contents of data.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from data import Posts
 
 app = Flask(__name__)
+
+Posts = Posts()
 
 @app.route('/')
 def index():
@@ -9,6 +12,10 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', posts = Posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,25 @@
+def Posts ():
+    posts = [
+        {
+            'id' : 1,
+            'title' : 'Post One',
+            'body' : 'The post body.',
+            'creator' : 'John Smith',
+            'date_posted' : '01-01-2001'
+        },
+        {
+            'id' : 2,
+            'title' : 'Post Two',
+            'body' : 'The post body.',
+            'creator' : 'Jane Doe',
+            'date_posted' : '01-01-2001'
+        },
+        {
+            'id' : 3,
+            'title' : 'Post Three',
+            'body' : 'The post body.',
+            'creator' : 'Peter Parker',
+            'date_posted' : '01-01-2001'
+        }
+    ]
+    return posts
